72f3fd42700cfa6637c0507edc6bc5c1e69be86f
Simon Leischnig <simon-jena@gmail.com>
2019-11-28 10:47:46 +0100

    remove files not needed anymore without bash env

--- bin/dev__runconfig_client.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import scriptlib
-
-loaded = scriptlib.load_script_from("/home/simon/sandbox/featurelist/sandbox/bin/dev__runconfig.py")
-loaded.spec.run(["--kv1=howdy", "arg1", "arg2"])
