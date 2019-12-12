3dd90a3f3ac4c137e15d007dd0d60d81ca38f02a
Simon Leischnig <simon-jena@gmail.com>
2019-12-12 15:48:24 +0100

    category extraction script works as intended

--- script_extract_ct2_cats.py
+++ script_extract_ct2_cats.py
@@ -21,28 +21,53 @@ import benedict; from benedict import benedict as bdict
 
 
 scsv_frame_ct2 = flist.SCSV_Dataset.Dataframe_From_Files([Path("/home/simon/sandbox/featurelist/ct_functionlist/ws/data/en/scsv_webdump/ct2.csv")])
-dataset = flist.SCSV_Dataset.From_Dataframe(scsv_frame_ct2)
-for row in dataset.get_rows():
-    funct = row["functionality"]
-    howimpl_payload = row["how_implemented"]
-    how_implemented = f"CT2:{howimpl_payload}"
-    normalizedpath = " \\ ".join([how_implemented] + row["path"])
-    hash = flist.makeId(funct, normalizedpath, howimpl_payload)
 
-    print(f"hash:{hash}", row)
+def scsv_dump_file(tool, language):
+    return Path(__file__).parent / f"ws-static/data/{language}/scsv_webdump/{tool.lower()}.csv"
+
+def scsv_reference_file(tool):
+    return scsv_dump_file(tool, "en")
+
+def category_table_output(tool, language):
+    return Path(__file__).parent / f"ws-static/categories_{tool.lower()}_{language}.csv"
+
+def load_legacy_and_infer_ids(tool, language) -> flist.SCSV_Dataset:
+    file = scsv_dump_file(tool, language)
+    referenceFile = scsv_reference_file(tool)
+
+    print(f"{file}, {referenceFile}")
+    dataset = flist.SCSV_Dataset.From_Dataframe(flist.SCSV_Dataset.Dataframe_From_Files([file]))
+    dataset_reference = flist.SCSV_Dataset.From_Dataframe(flist.SCSV_Dataset.Dataframe_From_Files([referenceFile]))
+    if len(dataset_reference.rows) != len(dataset.rows):
+        raise io.FlistException(f"could not extract legacy categories for {file} automatically: reference file {referenceFile} has a mismatching number of entries.")
+
+    entryIdx = 0
+    for referenceEntry in dataset_reference.rows:
+        # print(f"dbg: inferring {referenceEntry}")
+        referenceEntry.infer_id_from_fields(tool)
+        dataset.rows[entryIdx].id = referenceEntry.id
+        entryIdx += 1
+
+    return dataset
+
+def legacy_scsv_to_catref(scsv: flist.SCSV_Dataset) -> pandas.DataFrame:
+    df = scsv.to_dataframe()
+    df = df.drop(columns="path")
+    df = df.drop(columns="how_implemented")
+    df = df.drop(columns="functionality")
+    return df
+
+
+def extract_catref_file_in_wsstatic(tool, language):
+    legacy_with_ids = load_legacy_and_infer_ids(tool, language)
+    catref_df = legacy_scsv_to_catref(legacy_with_ids)
+    output = category_table_output(tool, language)
+    catref_df.to_csv(output, sep=flist.CSV_SEP, index=False, header=False)
+    print(f"written to {output}")
+
+extract_catref_file_in_wsstatic(tool = "CT2", language = "en")
+extract_catref_file_in_wsstatic(tool = "JCT", language = "en")
 
 # with open("/home/simon/sandbox/featurelist/ct_functionlist/ws-static/categories_ct2_en.csv", "wb") as opened:
 #     for entry in scsv_ct2.
 
-# || hash:69fda4c8 {'id': 'CT2:static:1', 'functionality': '3DES', 'how_implemented': 'CT2:C', 'path': ['CT2:C', 'Modern Ciphers', 'Symmetric', 'DES'], 'category': '2) Modern Ciphers'}
-# || hash:d080b739 {'id': 'CT2:static:2', 'functionality': '3DES Brute-Force Attack', 'how_implemented': 'CT2:C', 'path': ['CT2:C', 'Cryptanalysis', 'Specific', 'KeySearcher'], 'category': '7) Modern Cryptanalysis'}
-# || hash:140dee79 {'id': 'CT2:static:3', 'functionality': '3DES Brute-Force Attack', 'how_implemented': 'CT2:T', 'path': ['CT2:T', 'Cryptanalysis', 'Modern', 'Triple DES Brute-force Analysis'], 'category': '7) Modern Cryptanalysis'}
-# || hash:9f99a795 {'id': 'CT2:static:4', 'functionality': 'Achterbahn', 'how_implemented': 'CT2:C', 'path': ['CT2:C', 'Modern Ciphers', 'Symmetric', 'Achterbahn'], 'category': '2) Modern Ciphers'}
-# || hash:bc606c12 {'id': 'CT2:static:5', 'functionality': 'Achterbahn', 'how_implemented': 'CT2:T', 'path': ['CT2:T', 'Cryptography', 'Modern', 'Symmetric', 'Achterbahn Cipher'], 'category': '2) Modern Ciphers'}
-# || hash:d043a16a {'id': 'CT2:static:6', 'functionality': 'ADFGVX', 'how_implemented': 'CT2:C', 'path': ['CT2:C', 'Classic Ciphers', 'ADFGVX'], 'category': '1) Classic Ciphers'}
-# || hash:7b453c59 {'id': 'CT2:static:7', 'functionality': 'ADFGVX', 'how_implemented': 'CT2:T', 'path': ['CT2:T', 'Cryptanalysis', 'Classical', 'ADFGVX Cipher dictionary attack'], 'category': '1) Classic Ciphers'}
-# || hash:5ec789a9 {'id': 'CT2:static:8', 'functionality': 'ADFGVX', 'how_implemented': 'CT2:T', 'path': ['CT2:T', 'Cryptography', 'Classical', 'ADFGVX Cipher'], 'category': '1) Classic Ciphers'}
-# || hash:f5ad740f {'id': 'CT2:static:9', 'functionality': 'ADFGVX', 'how_implemented': 'CT2:W', 'path': ['CT2:W', 'Encryption', 'Decryption', 'Classic Encryption', 'Decryption', 'ADFGVX'], 'category': '1) Classic Ciphers'}
-# || hash:2e103daf {'id': 'CT2:static:657', 'functionality': 'Zero Knowledge Protocol', 'how_implemented': 'CT2:C', 'path': ['CT2:C', 'Protocols', 'Zero Knowledge Prover'], 'category': '13) Protocols'}
-# || hash:8f2154a8 {'id': 'CT2:static:658', 'functionality': 'Zero Knowledge Protocol', 'how_implemented': 'CT2:C', 'path': ['CT2:C', 'Protocols', 'Zero Knowledge Verifier'], 'category': '13) Protocols'}
-# || hash:5fde8b3a {'id': 'CT2:static:659', 'functionality': 'Zero Knowledge Protocol', 'how_implemented': 'CT2:T', 'path': ['CT2:T', 'Protocols', 'Zero Knowledge Protocol'], 'category': '13) Protocols'}
